988dbac8f215fab8937375cbfb6e133969ed9049
carlosn95 <gordillocarlosn@gmail.com>
2026-03-05 01:56:57 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # youtube-sentiment-insights
+
+conda create -n youtube python=3.11 -y
+
+conda activate youtube
+
+pip install -r requirements.txt
+
+## DVC
+
+dvc init
+dvc repro
+dvc dag
+
+## AWS
+aws configure
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version = '0.0.0',
+    author = 'Carlos',
+    author_email = 'carlos@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
